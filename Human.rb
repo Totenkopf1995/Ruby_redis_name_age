dfb76be60f6618fb27bee4c390c3991b898821b5
totenkopf1995 <uroxacin1@hotmail.com>
2023-12-16 00:17:37 -0500

    modificacion en main.rb y Human.rb

--- Human.rb
+++ Human.rb
@@ -1,10 +1,18 @@
 # Human.rb
 
+# Funcion para pedir informacion al usuario
+def obtener_input(prompt)
+  print "#{prompt}: "
+  gets.chomp
+end
+
+# Funcion para guadrar la informacion en redis
 def keep(redis, id, name, age)
   redis.hset("person:#{id}", 'name', name)
   redis.hset("person:#{id}", 'age', age.to_s)
 end
 
+# Funcion para mostrar la informacion de redis
 def show(redis, id)
   name = redis.hget("person:#{id}", 'name')
   age = redis.hget("person:#{id}", 'age')
@@ -16,11 +24,13 @@ def show(redis, id)
   result
 end
 
+# Funcion para eliminar ingformacion de redis
 def eliminate(redis, id)
   redis.del("person:#{id}")
 
 end
 
+# Funcion para verificar la existencia del ID
 def id_exists?(redis, id)
   puts "Debug: Verificando existencia de la clave 'person:#{id}' en la base de datos."
   redis.hexists("person:#{id}", 'name')
--- main.rb
+++ main.rb
@@ -3,34 +3,67 @@
 require 'redis'
 require_relative 'Human'
 
-def obtener_input(prompt)
-  print "#{prompt}: "
-  gets.chomp
-end
-
 begin
   redis = Redis.new(host: 'localhost', port: 6379, db: 0)
 
-  # Obtener información para una nueva persona
-  id = obtener_input('Indica el ID').to_i
-  name = obtener_input('Indica el nombre')
-  age = obtener_input('Indica la edad').to_i
+  # Preguntar al usuario que desea relizar
+  puts "Que desea realizar en este momento?: \n
+            1 = Guardar informacion\n
+            2 = Mostrar informacion\n
+            3 = Eliminar informacion"
 
-  # Verificar si el ID ya está en uso utilizando la conexión redis en Human.rb
-  if redis.hexists("person:#{id}", 'name')
-    puts "Debug: La clave 'person:#{id}' ya existe en la base de datos."
-    puts "Error: El ID #{id} ya está en uso. Por favor, elige otro ID."
-  else
-    # Guardar información y mostrar mensaje
-    keep(redis, id, name, age)
-    puts "\nGuardando..."
-    sleep 4
-    puts "\nInformación guardada para la persona con ID #{id}\n"
+  information = gets.chomp.to_i
 
-    # Mostrar información para la persona recién guardada
+  # Validar la descion tomada por el usuario
+  if information == 1
+    # Obtener información para una nueva persona
+    puts '=' * 30
+    id = obtener_input('Indica el ID').to_i
+    puts '=' * 30
+    name = obtener_input('Indica el nombre')
+    age = obtener_input('Indica la edad').to_i
+    puts
+
+    # Verificar si el ID ya está en uso utilizando la conexión redis en Human.rb
+    if redis.hexists("person:#{id}", 'name')
+      puts "Debug: La clave 'person:#{id}' ya existe en la base de datos."
+      puts "Error: El ID #{id} ya está en uso. Por favor, elige otro ID."
+    else
+      # Guardar información y mostrar mensaje
+      keep(redis, id, name, age)
+      puts "Guardando..."
+      sleep 4
+      puts "\nInformación guardada para la persona con ID #{id}\n"
+
+      # Mostrar información para la persona recién guardada
+      result = show(redis, id)
+      puts result
+    end
+  elsif information == 2
+    # Mostrar información para la persona
+    puts '=' * 30
+    print 'Cual ID deseas ver: '
+    id = gets.chomp
+
+    # Linea de separacion
+    puts '=' * 30
+
     result = show(redis, id)
     puts result
+  elsif information == 3
+    puts 'Cual ID que desea borrar: '
+    id = gets.chomp
+    eliminate(redis, id)
+    puts 'Borrando...'
+    sleep 4
+    puts "ID #{id} eliminado"
+  else
+    puts 'Opcion no valida'
   end
+
+
+
+
 
 rescue StandardError => e
   puts "Error: #{e.message}"
